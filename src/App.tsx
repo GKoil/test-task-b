84f82cee1de64c61e3d96b8a6da0038ca9471868
Kirill <samsuu@ya.ru>
2022-10-13 01:44:01 +0500

    feature add render table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Table from './components/Table';
+import service from './api';
+import { ADD } from './action-types';
 
 const App = () => {
-  return <div>app content</div>;
+  const dispatch = useDispatch();
+  // @ts-ignore TODO: fix ts-ignore
+  const tableData = useSelector(state => state.tableData) as TableItem[];
+
+  const [status, setStatus] = useState<
+    'idle' | 'pending' | 'success' | 'error'
+  >('idle');
+
+  useEffect(() => {
+    setStatus('pending');
+    service
+      .getTable()
+      .then(data => {
+        setStatus('success');
+        dispatch({ type: ADD, payload: data });
+      })
+      .catch(error => {
+        setStatus('error');
+      });
+  }, []);
+
+  return (
+    <>
+      {status === 'pending' && <div>spinner</div>}
+      {/* @ts-ignore */}
+      {status === 'success' && <Table itemsData={tableData} />}
+      {status === 'error' && <div>error</div>}
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { TableItem } from '../../types/tableData.type';
+
+type TableType = {
+  itemsData: TableItem[];
+};
+
+const groupByHeader = (objects: TableItem[]) => {
+  const GROUP_KEY = 'userId';
+
+  return objects.reduce((acc, object) => {
+    const groupName = object[GROUP_KEY];
+    const group = acc[groupName] ?? [];
+    return { ...acc, [groupName]: [...group, object] };
+  }, {} as { [key: string]: TableItem[] });
+};
+
+const Table = ({ itemsData }: TableType) => {
+  const processedData = groupByHeader(itemsData);
+  const processedContent = Object.values(processedData);
+  // TODO: Выделить в функцию
+  const processedContentRender = processedContent[0].map((val, index) =>
+    processedContent.map(row => row[row.length - 1 - index])
+  );
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(processedData).map(item => (
+            <th key={item}>{item}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {processedContentRender.map((item, index) => {
+          return (
+            <tr key={index}>
+              {item.map(({ title, id }) => (
+                <td key={id}>{title}</td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
